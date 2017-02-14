718f9d98d7ccfa0419902e0f2e34c5e2b358af85
Ed <oflynned@tcd.ie>
2017-02-14 15:28:27 +0000

    working retrieval of data and joins from mongodb

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,42 @@
 
 let express = require('express');
 let router = express.Router();
+let assert = require("assert");
+let Promise = require("promise");
+let mongo = require('mongodb').MongoClient;
 
-let json_loader = require("./helpers/json_loader");
+router.get('/metrics/:repo/:collection', function (req, res) {
+    let repo = req.params["repo"];
+    let metricsCollection = req.params["collection"];
 
-const AVG_COMPLEXITY = "TrumpScript_average_complexity_metrics";
-const CYCLOMATIC = "TrumpScript_cyclomatic_metrics";
-const MAINTAINABILITY = "TrumpScript_maintainability_metrics";
-const RAW_METRICS = "TrumpScript_raw_metrics";
-
-router.get('/avg-complexity', function (req, res) {
-    json_loader.readJSON(AVG_COMPLEXITY, data => res.json(data));
-});
-
-router.get('/avg-complexity-groomed', function (req, res) {
-    let dataItems = [];
-    json_loader.readJSON(AVG_COMPLEXITY, data =>
-        json_loader.groomAvgComplexity(dataItems, data, function () {
-            res.json(dataItems)
-        })
-    );
-});
-
-router.get('/graph', function (req, res) {
-    let dataItems = [];
-
-    json_loader.readJSON(AVG_COMPLEXITY, data =>
-        json_loader.groomAvgComplexity(dataItems, data, function () {
-            json_loader.writeJSON(AVG_COMPLEXITY, JSON.stringify(dataItems, null, 4), res.render("index", {
-                name: AVG_COMPLEXITY
-            }));
-        })
-    );
-});
-
-router.get("/:file", function (req, res) {
-    json_loader.readJSONSelf(req.params["file"], function (data) {
-        res.json(data)
+    mongo.connect('mongodb://localhost:27017/' + repo, function (err, db) {
+        let collection = db.collection(metricsCollection);
+        collection.find().toArray(function (err, results) {
+            res.json(results);
+            db.close();
+        });
     });
+});
+
+router.get("/avg-complexity-graph/:repo", function (req, res) {
+    mongo.connect('mongodb://localhost:27017/' + req.params["repo"], function (err, db) {
+        db.collection("average_complexity").aggregate({
+            $lookup: {
+                from: "commits",
+                localField: "commit_head",
+                foreignField: "head",
+                as: "commit_details"
+            }
+        }, function (err, result) {
+            res.json(result);
+        });
+    });
+});
+
+router.get("/graph/:repo/:collection", function (req, res) {
+    let repo = req.params["repo"];
+    let collection = req.params["collection"];
+    res.render("index", {url: "http://localhost:3000/metrics/" + repo + "/" + collection});
 });
 
 module.exports = router;
