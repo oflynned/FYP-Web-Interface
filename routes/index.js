d73fa23e2b797fe76a81465c59ad5bd0150b0397
Ed <oflynned@tcd.ie>
2017-02-16 14:08:54 +0000

    added lots of graphs for determining commits v time v leaders

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@
 let express = require('express');
 let router = express.Router();
 let assert = require("assert");
-let Promise = require("promise");
 let mongo = require('mongodb').MongoClient;
 
 router.get('/metrics/:repo/:collection', function (req, res) {
@@ -19,7 +18,7 @@ router.get('/metrics/:repo/:collection', function (req, res) {
     });
 });
 
-router.get("/avg-complexity-graph/:repo", function (req, res) {
+router.get("/avg-complexity-data/:repo", function (req, res) {
     mongo.connect('mongodb://localhost:27017/' + req.params["repo"], function (err, db) {
         db.collection("average_complexity").aggregate({
             $lookup: {
@@ -34,10 +33,94 @@ router.get("/avg-complexity-graph/:repo", function (req, res) {
     });
 });
 
-router.get("/graph/:repo/:collection", function (req, res) {
+router.get("/cyclomatic-complexity-data/:repo", function (req, res) {
+    mongo.connect('mongodb://localhost:27017/' + req.params["repo"], function (err, db) {
+        db.collection("cyclomatic_complexity").aggregate({
+            $lookup: {
+                from: "commits",
+                localField: "commit_head",
+                foreignField: "head",
+                as: "commit_details"
+            }
+        }, function (err, result) {
+            res.json(result);
+        });
+    });
+});
+
+router.get("/maintainability-data/:repo", function (req, res) {
+    mongo.connect('mongodb://localhost:27017/' + req.params["repo"], function (err, db) {
+        db.collection("maintainability").aggregate({
+            $lookup: {
+                from: "commits",
+                localField: "commit",
+                foreignField: "head",
+                as: "commit_details"
+            }
+        }, function (err, result) {
+            res.json(result);
+        });
+    });
+});
+
+router.get("/committers-data/:repo", function (req, res) {
+    mongo.connect('mongodb://localhost:27017/' + req.params["repo"], function (err, db) {
+        db.collection("commits").find({}).toArray(function (err, result) {
+            res.json(result);
+        });
+    });
+});
+
+// graph rendering
+
+router.get("/avg-graph/:repo", function (req, res) {
     let repo = req.params["repo"];
     let collection = req.params["collection"];
-    res.render("index", {url: "http://localhost:3000/metrics/" + repo + "/" + collection});
+
+    res.render("avg-complexity", {
+        repo: repo,
+        collection: collection
+    });
+});
+
+router.get("/complexity-graph/:repo", function (req, res) {
+    let repo = req.params["repo"];
+    let collection = req.params["collection"];
+
+    res.render("complexity", {
+        repo: repo,
+        collection: collection
+    });
+});
+
+router.get("/maintainability-graph/:repo", function (req, res) {
+    let repo = req.params["repo"];
+    let collection = req.params["collection"];
+
+    res.render("maintainability", {
+        repo: repo,
+        collection: collection
+    });
+});
+
+router.get("/committers-graph/:repo", function (req, res) {
+    let repo = req.params["repo"];
+    let collection = req.params["collection"];
+
+    res.render("committers", {
+        repo: repo,
+        collection: collection
+    });
+});
+
+router.get("/commit-frequency-graph/:repo", function (req, res) {
+    let repo = req.params["repo"];
+    let collection = req.params["collection"];
+
+    res.render("commit-frequency", {
+        repo: repo,
+        collection: collection
+    });
 });
 
 module.exports = router;
